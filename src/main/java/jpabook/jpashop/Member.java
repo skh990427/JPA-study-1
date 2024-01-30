bc43cc517a8064758569103f7fd1f7c7bc8b14d4
KwangHo <skh990427@naver.com>
2024-01-30 09:09:58 +0900

    @Generated Value 버전 재설정

--- src/main/java/jpabook/jpashop/Member.java
+++ src/main/java/jpabook/jpashop/Member.java
@@ -2,7 +2,6 @@ package jpabook.jpashop;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,7 +11,7 @@ import lombok.Setter;
 @Setter
 public class Member {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue
     private Long id;
 
     private String username;
