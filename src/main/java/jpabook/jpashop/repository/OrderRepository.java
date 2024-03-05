6151f7f19ce36635abe99ebeffd755a479e3bbe0
KwangHo <skh990427@naver.com>
2024-03-05 15:24:54 +0900

    주문 Repository 개발

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(Ordersearch ordersearch) //이친구는 나중에 뒷 챕터에서 개발예정(동적쿼리)
+}
