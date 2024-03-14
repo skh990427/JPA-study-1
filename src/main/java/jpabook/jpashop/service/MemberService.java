1ddfaa856a86c2541643517973f0a66da1fc5cfd
KwangHo <skh990427@naver.com>
2024-03-14 11:14:33 +0900

    스프링 Data Jpa 소개

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -41,12 +41,12 @@ public class MemberService {
 
     //회원 id로 단건 조회
     public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
+        return memberRepository.findById(memberId).get();
     }
 
     @Transactional
     public void update(Long id, String name) {
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
     }
 }
